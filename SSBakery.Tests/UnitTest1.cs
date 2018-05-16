98fe2edb095446d14a3c8067f9367298596faebc
Colt Bauman <cabauman@maverickgames.co>
2018-05-16 15:33:34 -0500

    Add project files.

---
+++ SSBakery.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using SSBakery.ViewModels;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace SSBakery.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            ItemsViewModel vm = new ItemsViewModel();
+        }
+    }
+}
---
+++ SSBakery/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+using SSBakery.Views;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation (XamlCompilationOptions.Compile)]
+namespace SSBakery
+{
+	public partial class App : Application
+	{
+		
+		public App ()
+		{
+			InitializeComponent();
+
+
+			MainPage = new MainPage();
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
