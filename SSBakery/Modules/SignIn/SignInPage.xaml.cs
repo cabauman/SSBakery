30938ada05811324f0b9d0f99c745121b21ec1d2
Colt Bauman <cabauman@maverickgames.co>
2018-06-03 14:51:18 -0500

    Add sign in page

---
+++ SSBakery/Modules/SignIn/SignInPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reactive.Disposables;
+using ReactiveUI;
+using SSBakery.UI.Common;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SSBakery.UI.Modules
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SignInPage : ContentPageBase<SignInViewModel>
+    {
+        public SignInPage()
+        {
+            InitializeComponent();
+
+            this.WhenActivated(
+                disposables =>
+                {
+                    this
+                        .BindCommand(ViewModel, vm => vm.GoToMainPage, v => v.ContinueAnonymouslyButton)
+                        .DisposeWith(disposables);
+                });
+        }
+    }
+}
---
+++ SSBakery/Modules/SignIn/SignInViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reactive;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using ReactiveUI;
+using Splat;
+using SSBakery;
+using SSBakery.UI.Common;
+
+namespace SSBakery.UI.Modules
+{
+    public class SignInViewModel : ViewModelBase
+    {
+        public SignInViewModel(IScreen hostScreen = null)
+            : base(hostScreen)
+        {
+        }
+
+        public ReactiveCommand GoToMainPage { get; }
+
+        public IObservable<Unit> GoToPage(IRoutableViewModel routableViewModel)
+        {
+            return HostScreen
+                .Router
+                .Navigate
+                .Execute(routableViewModel)
+                .Select(_ => Unit.Default);
+        }
+    }
+}
