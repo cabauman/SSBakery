e54b8b3023d90366e409a99341797839c6ac23b1
Colt Bauman <cabauman@maverickgames.co>
2018-08-18 20:52:21 +0900

    Add Firebase storage service

---
+++ SSBakery.Core/Services/FirebaseStorageService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using SSBakery.Services.Interfaces;
+
+namespace SSBakery.Services
+{
+    public class FirebaseStorageService : IFirebaseStorageService
+    {
+        private const string URL = "";
+
+        public IObservable<string> GetDownloadUrl(string filename)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
