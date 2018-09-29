8dbd12bd1def03a30d6c2bdb3d71306452b04b57
cabauman <cab452005@hotmail.com>
2018-09-29 16:26:14 +0900

    Add services for FCM

---
+++ SSBakery.Android/MyFirebaseMessagingService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Media;
+using Android.Support.V4.App;
+using Firebase.Messaging;
+
+namespace SSBakery.Droid
+{
+    [Service]
+    [IntentFilter(new [] { "com.google.firebase.MESSAGING_EVENT" })]
+    public class MyFirebaseMessagingService : FirebaseMessagingService
+    {
+        private const string TAG = "MyFirebaseMsgService";
+
+        /// <summary>
+        /// Called when message is received.
+        /// </summary>
+        /// <param name="message">The remote message that was received.</param>
+        public override void OnMessageReceived(RemoteMessage message)
+        {
+            // TODO(developer): Handle FCM messages here.
+            // If the application is in the foreground handle both data and notification messages here.
+            // Also if you intend on generating your own notifications as a result of a received FCM
+            // message, here is where that should be initiated. See sendNotification method below.
+            Android.Util.Log.Debug(TAG, "From: " + message.From);
+            Android.Util.Log.Debug(TAG, "Notification Message Body: " + message.GetNotification().Body);
+        }
+
+        /// <summary>
+        /// Create and show a simple notification containing the received FCM message.
+        /// </summary>
+        /// <param name="messageBody">The message to be displayed.</param>
+        private void SendNotification(string messageBody)
+        {
+            var intent = new Intent(this, typeof(MainActivity));
+            intent.AddFlags(ActivityFlags.ClearTop);
+            var pendingIntent = PendingIntent.GetActivity(this, 0 /* Request code */, intent, PendingIntentFlags.OneShot);
+
+            var defaultSoundUri = RingtoneManager.GetDefaultUri(RingtoneType.Notification);
+            var notificationBuilder = new NotificationCompat.Builder(this)
+                //.SetSmallIcon(Resource.Drawable.ic_stat_ic_notification)
+                .SetContentTitle("FCM Message")
+                .SetContentText(messageBody)
+                .SetAutoCancel(true)
+                .SetSound(defaultSoundUri)
+                .SetContentIntent(pendingIntent);
+
+            var notificationManager = NotificationManager.FromContext(this);
+
+            notificationManager.Notify(0 /* ID of notification */, notificationBuilder.Build());
+        }
+    }
+}
