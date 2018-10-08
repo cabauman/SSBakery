a91f881c2f07f72dd0a4986c042dfdb7c6e268f5
cabauman <cab452005@hotmail.com>
2018-10-08 14:11:44 +0900

    Add generic object pool

---
+++ SSBakery.Core/Helpers/ObjectPool.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace SSBakery.Helpers
+{
+    public class ObjectPool<T>
+    {
+        private ConcurrentBag<T> _objects;
+        private Func<T> _objectGenerator;
+
+        public ObjectPool(Func<T> objectGenerator)
+        {
+            _objectGenerator = objectGenerator ?? throw new ArgumentNullException("objectGenerator");
+            _objects = new ConcurrentBag<T>();
+        }
+
+        public T GetObject()
+        {
+            if(_objects.TryTake(out var item))
+            {
+                return item;
+            }
+
+            return _objectGenerator();
+        }
+
+        public void PutObject(T item)
+        {
+            _objects.Add(item);
+        }
+    }
+}
