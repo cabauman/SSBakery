4346e36a0b59bc3e572320ee8e12c55f4f8d7ee8
cabauman <cab452005@hotmail.com>
2018-10-13 14:35:54 +0900

    Rename customer repo to rewards member

---
+++ SSBakery.Core/Repositories/Interfaces/IRewardsMemberRepo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using GameCtor.Repository;
+using SSBakery.Models;
+
+namespace SSBakery.Repositories.Interfaces
+{
+    public interface IRewardsMemberRepo : IRepository<RewardsMember>
+    {
+        IObservable<FirebaseEvent<RewardsMember>> Observe();
+    }
+}
---
+++ SSBakery.Core/Repositories/RewardsMemberRepo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Firebase.Database;
+using GameCtor.FirebaseDatabase.DotNet;
+using SSBakery.Models;
+using SSBakery.Repositories.Interfaces;
+
+namespace SSBakery.Repositories
+{
+    public class RewardsMemberRepo : FirebaseOfflineRepo<RewardsMember>, IRewardsMemberRepo
+    {
+        public RewardsMemberRepo(FirebaseClient client, string path, string key = null)
+            : base(client, path, key)
+        {
+        }
+    }
+}
